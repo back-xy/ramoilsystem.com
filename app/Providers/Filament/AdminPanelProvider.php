e1dbaab5cf6baa2a6ca49510167e51a27c073e22
Bawar <bawarmohamad100@gmail.com>
2026-01-11 07:10:56 +0300

    adding pwa manualy

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -12,6 +12,9 @@ use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets\AccountWidget;
 use App\Filament\Widgets\PlacesStatsWidget;
+use Filament\Support\Facades\FilamentView;
+use Filament\View\PanelsRenderHook;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -32,6 +35,20 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Blue,
             ])
+            ->renderHook(
+                PanelsRenderHook::HEAD_END,
+                fn() => Blade::render(<<<'HTML'
+                    <link rel="icon" type="image/png" href="{{ asset('images/favicon-96x96.png') }}" sizes="96x96" />
+                    <link rel="icon" type="image/svg+xml" href="{{ asset('images/favicon.svg') }}" />
+                    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" />
+                    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}" />
+                    <meta name="apple-mobile-web-app-title" content="ڕام ئۆیل" />
+                    <link rel="manifest" href="{{ asset('images/site.webmanifest') }}" />
+                    <meta name="theme-color" content="#007aff">
+                    <meta name="apple-mobile-web-app-capable" content="yes">
+                    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+                HTML)
+            )
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
